38cc3b7b5e10b9bf81e71dd4e8213f55bfd62305
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-03-03 21:54:39 -0600

    add: random trees branch

--- fractalConfig.js
+++ fractalConfig.js
@@ -1,16 +1,16 @@
 const getGlobalConfig = (props) => ({
     initial: {
-        distance: 100,
-        rotation: 0,
+        distance: 75,
+        rotation: 180,
         x: () => props.c.width / 2,
-        y: () => props.c.height / 2,
+        y: () => props.c.height,
         thickness: 6,
         color: '#f0f0f0',
     },
-    distanceDecrement: 0.7885,
+    distanceDecrement: 0.885,
     thicknessReductionStep: 0.3,
-    treesAmount: 5,
-    limit: 7,
+    treesAmount: 1,
+    limit: 9,
     distribution: 2,
     background: '#10101022',
     growingSpeed: 50
