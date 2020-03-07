8b81626b6dace4fc78141d2074f21811ae2d9cc4
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-03-07 08:58:35 -0600

    enhance: domEvents

--- js/domEvents.js
+++ js/domEvents.js
@@ -4,4 +4,4 @@ songIframe.style.display = 'none';
 songIframe.addEventListener('load', () => {
     songIframe.style.display = 'block';
     window.addEventListener('blur', () => songIframe.style.zIndex = -1);
-})
+});
