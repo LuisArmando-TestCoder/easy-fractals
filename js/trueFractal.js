1a9fd9638ded612c860f486182707d7ee5f982c4
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-03-07 08:02:21 -0600

    enhance: clean imports

--- js/trueFractal.js
+++ js/trueFractal.js
@@ -1,18 +1,14 @@
 import preset from '../canvas-preset/index.js';
-import getGlobalConfig from './globalConfig.js';
 import createForest from './createForest.js';
 import createSky from './createSky.js';
 
 preset(canvas => {
     canvas.size();
-
     const sky = createSky(canvas);
     let forest = createForest(canvas);
-
     let frame = 0;
 
     canvas.draw(() => {
-
         frame++;
 
         if(!(frame % 256)) {
