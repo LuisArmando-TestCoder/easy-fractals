20646d49590bc7452a5689395bd5f160a991794a
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-03-07 07:54:08 -0600

    enhance: gradient positions

--- js/trueFractal.js
+++ js/trueFractal.js
@@ -6,7 +6,6 @@ import createSky from './createSky.js';
 preset(canvas => {
     canvas.size();
 
-    const globalConfig = getGlobalConfig();
     const sky = createSky(canvas);
     let forest = createForest(canvas);
 
@@ -16,17 +15,17 @@ preset(canvas => {
 
         frame++;
 
-        if(!(frame % 512)) {
+        if(!(frame % 256)) {
             forest.splice(0);
             forest = createForest(canvas);
         }
 
         // canvas.clear(globalConfig.background);
         // canvas.clear();
-        var gradient = canvas.ctx.createRadialGradient(canvas.c.width / 2, canvas.c.height / 4, canvas.c.width, canvas.c.width / 2, -canvas.c.height / 4, canvas.c.width / 2);
-        gradient.addColorStop(0, ' #014a9022');
-        gradient.addColorStop(0.5, '#5565cc22');
-        gradient.addColorStop(1, '#948fd822');
+        var gradient = canvas.ctx.createRadialGradient(canvas.c.width / 2, -canvas.c.height / 4, canvas.c.width, canvas.c.width / 2, -canvas.c.height / 4, canvas.c.width / 2);
+        gradient.addColorStop(0, ' #014a9044');
+        gradient.addColorStop(0.5, '#5565cc44');
+        gradient.addColorStop(1, '#948fd844');
         canvas.ctx.fillStyle = gradient;
         canvas.ctx.fillRect(0, 0, canvas.c.width, canvas.c.height);
 
